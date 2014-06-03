0276050dcb81e12f3f07b771330a2d4640f33fcf
huubgitter <huubgitter@gmail.com>
2014-06-03 20:26:38 +0200

    Create css.js

---
+++ javascripts/css.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+      $('#therocktrading').click(function(){ 
+        window.open("https://www.therocktrading.com/en/pages/affiliate");
+        return false;
+      });
+      $('#ripplewise').click(function(){ 
+        window.open("https://www.ripplewise.com/buy/9c8d2dfc-68b3-4027-a2bf-daee5c351979/");
+        return false;
+      });
+});
