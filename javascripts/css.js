f96260ee93e319074349aa1f3e046b01f30273dc
huubgitter <huubgitter@gmail.com>
2014-08-11 17:20:58 +0200

    Update css.js

--- javascripts/css.js
+++ javascripts/css.js
@@ -7,4 +7,8 @@ $( document ).ready(function() {
         window.open("https://www.ripplewise.com/buy/9c8d2dfc-68b3-4027-a2bf-daee5c351979/");
         return false;
       });
+      $('#bitso').click(function(){ 
+        window.open("https://www.bitso.com/");
+        return false;
+      });
 });
